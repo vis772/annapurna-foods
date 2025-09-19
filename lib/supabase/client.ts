4ad74a34cf1c54e705e3b9178850c6eb5434a6e3
vis772 <167153331+vis772@users.noreply.github.com>
2025-09-19 00:55:19 -0500

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,5 +1,5 @@
 import { createBrowserClient } from "@supabase/ssr"
 
 export function createClient() {
-  return createBrowserClient(process.env.https://supabase.com/dashboard/project/myjfffgspplojkijldfs/!, process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15amZmZmdzcHBsb2praWpsZGZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNTYxOTQsImV4cCI6MjA3MzgzMjE5NH0.gHzWzUtBH1YRuEl9XZ3BMvx2Em52uU7-8fpwv9aSZ_Q!)
+  return createBrowserClient(process.env.https://supabase.com/dashboard/project/myjfffgspplojkijldfs/, process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15amZmZmdzcHBsb2praWpsZGZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNTYxOTQsImV4cCI6MjA3MzgzMjE5NH0.gHzWzUtBH1YRuEl9XZ3BMvx2Em52uU7-8fpwv9aSZ_Q)
 }
